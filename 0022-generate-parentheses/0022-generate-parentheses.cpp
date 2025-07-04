038213e303aa5f5b9f455bf3b1b4dbfd5164f3ab
Khushi Mantri <129003839+Kcode15@users.noreply.github.com>
2025-07-05 01:11:50 +0530

    Time: 4 ms (37.39%), Space: 15.7 MB (36.49%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void para(int open,int close,int n,string s,vector<string>& ans){
+        if(open==close && open+close==n*2){
+            ans.push_back(s);
+            return;
+        }
+        if(open<n){
+            para(open+1,close,n,s+"(",ans);
+        }
+        if(close<open){ //close cannot be present without open therefore we need to have some open ( to have close )
+            para(open,close+1,n,s+")",ans);
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        para(0,0,n,"",ans);
+        return ans;
+    }
+};
