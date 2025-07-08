957cb7b03d0ec32ad549549dd49e92272c57148e
Khushi Mantri <129003839+Kcode15@users.noreply.github.com>
2025-07-08 23:37:16 +0530

    Time: 0 ms (100%), Space: 14 MB (90.87%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,30 +1,24 @@
 class Solution {
 public:
-    void combSum(int i,int n,vector<int>& arr,vector<int>& ans,int currSum,vector<vector<int>>& final,int target){
+    void combSum(int i,int n,vector<int>& arr,vector<int>& ans,vector<vector<int>>& final,int target){
         if(i==n){
-            if(currSum==target){
+            if(target == 0){
                 final.push_back(ans);
             }
             return;
         }
-         if(currSum>target){
-            return;
-         } 
-        currSum = currSum+arr[i];
-        ans.push_back(arr[i]);
-        // if(currSum<target){
-        //     combSum(i,n,arr,ans,currSum,final,target);
-        // }
-        combSum(i,n,arr,ans,currSum,final,target);
-        currSum = currSum-arr[i];
-        ans.pop_back();
-        combSum(i+1,n,arr,ans,currSum,final,target);
+        if(arr[i]<=target){
+            ans.push_back(arr[i]);
+            combSum(i,n,arr,ans,final,target-arr[i]);
+            ans.pop_back();
+        }
+        combSum(i+1,n,arr,ans,final,target);
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         int n = candidates.size();
-        vector<vector<int>> f_ans;
         vector<int> ans;
-        combSum(0,n,candidates,ans,0,f_ans,target);
+        vector<vector<int>> f_ans;
+        combSum(0,n,candidates,ans,f_ans,target);
         return f_ans;
     }
 };
