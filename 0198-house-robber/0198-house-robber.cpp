54be9007f6ff4b6108a6ec9882a5ce89805ac81b
Khushi Mantri <129003839+Kcode15@users.noreply.github.com>
2025-07-21 01:29:11 +0530

    Time: 0 ms (100%), Space: 10.6 MB (64.2%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
-    int sumRob(int n,vector<int>& nums,vector<int>& dp){
-     if(n==0){
-        return nums[n];
-     }
-     if(n<0){
-        return 0;
-     }
-     if(dp[n]!=-1){
-        return dp[n];
-     }
-     int pick = nums[n]+sumRob(n-2,nums,dp);
-     int npick = sumRob(n-1,nums,dp);
-     return dp[n] = max(pick,npick);
-    }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n+1,-1);
-        int ans = sumRob(n-1,nums,dp);
-        return ans;
+        vector<int> dp(n,-1);
+        dp[0] = nums[0];
+        for(int i=1;i<n;i++){
+            int r1 = nums[i];
+            if(i>1){
+                r1 = r1+dp[i-2];
+            }
+            int r2 = dp[i-1];
+            dp[i]=max(r1,r2);
+        }
+        return dp[n-1];
     }
 };
