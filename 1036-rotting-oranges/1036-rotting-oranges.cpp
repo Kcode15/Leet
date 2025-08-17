cad051cc647271ec017df42dcee6c7ee72d2b3dd
Khushi Mantri <129003839+Kcode15@users.noreply.github.com>
2025-08-17 12:45:26 +0530

    Time: 0 ms (100%), Space: 16.5 MB (97.44%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -11,20 +11,20 @@ public:
                 }
             }
         }
+        vector<int> drow = {0,0,1,-1};
+        vector<int> dcol = {1,-1,0,0};
         int tm = 0;
-        vector<int> dx = {0,0,1,-1}; // for directions in row
-        vector<int> dy = {1,-1,0,0}; //  for direcions in column
         while(!q.empty()){
             int r = q.front().first.first;
             int c = q.front().first.second;
             tm = max(tm,q.front().second);
             q.pop();
             for(int i=0;i<4;i++){
-                int cx = r+dx[i]; // current row
-                int cy = c+dy[i]; // current column
-                if(cx>=0 && cx<n && cy>=0 && cy<m && grid[cx][cy]==1){ // Always check the condtion 
-                    q.push({{cx,cy},tm+1});  // here we are pushing the neighbouring index which is not visited and fresh
-                    grid[cx][cy]=2;
+                int crow = r+drow[i];
+                int ccol = c+dcol[i];
+                if(crow>=0 && crow<n && ccol>=0 && ccol<m && grid[crow][ccol]==1){
+                    q.push({{crow,ccol},tm+1});
+                    grid[crow][ccol]=2;
                 }
             }
         }
