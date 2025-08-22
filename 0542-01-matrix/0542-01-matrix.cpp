52cfc949fe53c16d59a8eaa3781003d6dc5b5323
Khushi Mantri <129003839+Kcode15@users.noreply.github.com>
2025-08-23 01:01:47 +0530

    Time: 39 ms (12.63%), Space: 38.9 MB (34.46%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -3,40 +3,35 @@ public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int n = mat.size();
         int m = mat[0].size();
-        
-        vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
-        queue<pair<int, int>> q;
-        
-        // Push all 0's into queue first
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (mat[i][j] == 0) {
-                    dist[i][j] = 0;
-                    q.push({i, j});
+        vector<vector<int>> distMatrix(n,vector<int>(m,0));
+        vector<vector<int>> vis(n,vector<int>(m,0));
+        queue<pair<pair<int,int>,int>> q;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==0){
+                    distMatrix[i][j] = 0;
+                    q.push({{i,j},0});
+                    vis[i][j]=1;
                 }
             }
         }
-        
-        vector<int> dx = {0, 0, 1, -1};
-        vector<int> dy = {1, -1, 0, 0};
-        
-        while (!q.empty()) {
-            auto [r, c] = q.front();
+        vector<int> dx = {0,0,1,-1};
+        vector<int> dy = {1,-1,0,0};
+        while(!q.empty()){
+            int r = q.front().first.first;
+            int c = q.front().first.second;
+            int cnt = q.front().second;
+            distMatrix[r][c]= cnt;
             q.pop();
-            
-            for (int i = 0; i < 4; i++) {
-                int nr = r + dx[i];
-                int nc = c + dy[i];
-                
-                if (nr >= 0 && nr < n && nc >= 0 && nc < m) {
-                    if (dist[nr][nc] > dist[r][c] + 1) {
-                        dist[nr][nc] = dist[r][c] + 1;
-                        q.push({nr, nc});
-                    }
+            for(int i=0;i<4;i++){
+                int cx = r+dx[i];
+                int cy = c+dy[i];
+                if(cx>=0 && cx<n && cy>=0 && cy<m && !vis[cx][cy]){
+                    q.push({{cx,cy},cnt+1});
+                    vis[cx][cy] =1;
                 }
             }
         }
-        
-        return dist;
+        return distMatrix;
     }
 };
