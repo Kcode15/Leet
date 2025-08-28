f834fb25da3ac9998ac6ae9c32f6cfaba22efac6
Khushi Mantri <129003839+Kcode15@users.noreply.github.com>
2025-08-29 00:16:52 +0530

    Time: 6 ms (37.79%), Space: 18.5 MB (48.19%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        int n = prerequisites.size();
+        unordered_map<int,vector<int>> graph;
+        for(int i=0;i<n;i++){
+            int a = prerequisites[i][0];
+            int b = prerequisites[i][1];
+            graph[a].push_back(b);
+        }
+        vector<int> indegree(numCourses,0);
+        for(int i=0;i<numCourses;i++){
+            for(auto it : graph[i]){
+                indegree[it]++;
+            }
+        }
+        
+        queue<int> q;
+        int cnt = 0;
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+        vector<int> ans;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+            // As node is in topo sort so remove it from indegree;
+            for(auto it: graph[node]){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.push(it);
+                }
+            }
+        }
+        if(ans.size()!=numCourses){
+            return {};
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
