21fcb9d091c0a4bec4bd8a0031b2b6b00c2e8e82
Khushi Mantri <129003839+Kcode15@users.noreply.github.com>
2025-09-01 00:24:37 +0530

    Time: 51 ms (42.97%), Space: 66.2 MB (50.5%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -1,37 +1,35 @@
 class Solution {
 public:
-    bool dfs(int node,vector<vector<int>>& graph,vector<int>& vis,vector<int>& pathVis){
-        vis[node]=1;
-        pathVis[node] =1;
-        for(auto ngb:graph[node]){
-            //when the node is not visited
-            if(!vis[ngb]){
-                if(dfs(ngb,graph,vis,pathVis) == true){
-                    return true;
-                };
-            }
-            // if the node has been previously visited
-            // but it has to be on same path;
-            else if(pathVis[ngb]){
-                return true;
-            }
-        }
-        vis[node] = 2;
-        pathVis[node] = 0;
-        return false;
-    }
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int V = graph.size();
-        vector<int> pathVis(V,0);
-        vector<int> vis(V,0);
+        vector<int> indegree(V,0);
+        vector<vector<int>> revGraph(V);
         for(int i=0;i<V;i++){
-            if(!vis[i]){
-                dfs(i,graph,vis,pathVis);
+            for(auto it : graph[i]){
+                revGraph[it].push_back(i);
+                indegree[i]++;
+            }
+        }
+        queue<int> q;
+        for(int i=0;i<V;i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            // As node is in topo sort so remove it from indegree;
+            for(auto it: revGraph[node]){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.push(it);
+                }
             }
         }
         vector<int> ans;
         for(int i=0;i<V;i++){
-            if(vis[i]==2){
+            if(indegree[i]==0){
                 ans.push_back(i);
             }
         }
