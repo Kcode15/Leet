f02e6ae2f217b43e1a7d8e093a02dc4b33199cf5
Khushi Mantri <129003839+Kcode15@users.noreply.github.com>
2025-08-31 16:56:12 +0530

    Time: 11 ms (71.17%), Space: 52.3 MB (84.18%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -1,26 +1,39 @@
 class Solution {
 public:
-    bool dfs(int node, vector<vector<int>>& graph, vector<int>& state) {
-        if (state[node] != 0) {
-            return state[node] == 2; // already determined safe
+    bool dfs(int node,vector<vector<int>>& graph,vector<int>& vis,vector<int>& pathVis){
+        vis[node]=1;
+        pathVis[node] =1;
+        for(auto ngb:graph[node]){
+            //when the node is not visited
+            if(!vis[ngb]){
+                if(dfs(ngb,graph,vis,pathVis) == true){
+                    vis[node] = 1;
+                    return true;
+                };
+            }
+            // if the node has been previously visited
+            // but it has to be on same path;
+            else if(pathVis[ngb]){
+                vis[node] = 1;
+                return true;
+            }
         }
-        
-        state[node] = 1; // visiting
-        for (int nei : graph[node]) {
-            if (state[nei] == 1) return false; // cycle found
-            if (!dfs(nei, graph, state)) return false;
-        }
-        state[node] = 2; // safe
-        return true;
+        vis[node] = 2;
+        pathVis[node] = 0;
+        return false;
     }
-    
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int n = graph.size();
-        vector<int> state(n, 0); // 0=unvisited, 1=visiting, 2=safe
+        int V = graph.size();
+        vector<int> pathVis(V,0);
+        vector<int> vis(V,0);
+        for(int i=0;i<V;i++){
+            if(!vis[i]){
+                dfs(i,graph,vis,pathVis);
+            }
+        }
         vector<int> ans;
-        
-        for (int i = 0; i < n; i++) {
-            if (dfs(i, graph, state)) {
+        for(int i=0;i<V;i++){
+            if(vis[i]==2){
                 ans.push_back(i);
             }
         }
