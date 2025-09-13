63260b64d0b96aa38d14266a185c160f926cb48b
Khushi Mantri <129003839+Kcode15@users.noreply.github.com>
2025-09-13 13:30:01 +0530

    Time: 0 ms (100%), Space: 9.9 MB (44.54%) - LeetHub

--- 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.cpp
+++ 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.cpp
@@ -3,17 +3,15 @@ public:
     int maxFreqSum(string s) {
         int n = s.size();
         unordered_map<char,int> umap;
-        for(char ch: s){
-            umap[ch]++;
-        }
-        int maxi1 = 0,maxi2 = 0;
-        for(auto it:umap){
-            char ch = it.first;
-            if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
-                maxi1 = max(maxi1,it.second);
+        int maxi1 = 0,maxi2 =0;
+        for(int i=0;i<n;i++){
+            if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'){
+                umap[s[i]]++;
+                maxi1 = max(maxi1,umap[s[i]]);
             }
             else{
-                maxi2 = max(maxi2,it.second);
+                umap[s[i]]++;
+                maxi2=max(maxi2,umap[s[i]]);
             }
         }
         return maxi1+maxi2;
