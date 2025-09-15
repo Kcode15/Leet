7f374e66bedffcf8131e0e1037c5e2e3a3db92ad
Khushi Mantri <129003839+Kcode15@users.noreply.github.com>
2025-09-16 00:04:57 +0530

    Time: 0 ms (100%), Space: 17.6 MB (17.75%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-    int firstOcc(vector<int> nums,int target){
+    int firstOcc(vector<int>& nums,int target){
         int n = nums.size();
         int start = 0;
         int end = n-1;
-        int mid = start+(end-start)/2;
+        int mid = start + (end-start)/2;
         int ans = -1;
         while(start<=end){
-            if(nums[mid] == target){
+            if(nums[mid]==target){
                 ans = mid;
-                end = mid -1;
+                end = mid - 1;
             }
             else if(target>nums[mid]){
                 start = mid+1;
@@ -17,33 +17,32 @@ public:
             else{
                 end = mid-1;
             }
-            mid=start+(end-start)/2;
+            mid = start+(end-start)/2;
         }
         return ans;
     }
-    int lastOcc(vector<int> nums,int target){
+    int lastOcc(vector<int>& nums,int target){
         int n = nums.size();
         int start = 0;
         int end = n-1;
-        int mid = start+(end-start)/2;
+        int mid = start + (end - start)/2;
         int ans = -1;
         while(start<=end){
-            if(nums[mid] == target){
+            if(nums[mid]==target){
                 ans = mid;
-                start = mid + 1;
-            }
-            else if(target>nums[mid]){
                 start = mid+1;
             }
-            else{
+            else if(target<nums[mid]){
                 end = mid-1;
             }
-            mid=start+(end-start)/2;
+            else{
+                start = mid+1;
+            }
+            mid = start + (end - start)/2;
         }
         return ans;
     }
     vector<int> searchRange(vector<int>& nums, int target) {
-        int n = nums.size();
         vector<int> ans;
         int first = firstOcc(nums,target);
         int last = lastOcc(nums,target);
